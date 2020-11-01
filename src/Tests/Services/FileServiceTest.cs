e3a1090cd377f1d2cbc61b5c64f4d9e96b698449
Gabriel Lucena <gnllucena@gmail.com>
2020-10-31 21:46:29 -0300

    Testes unitários.

---
+++ src/Tests/Services/FileServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tests.Services
+{
+    public class FileServiceTest
+    {
+        [Fact(DisplayName = "Test")]
+        public void Test()
+        {
+
+        }
+    }
+}
